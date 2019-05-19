18aa0c2df2008e27a450c13d58c20f46c6aebfd5
FnSGit <2696421750@qq.com>
2019-05-19 23:12:47 +0800

    initialize

--- src/main/java/com/fsyj/epos/controller/login/LoginController.java
+++ src/main/java/com/fsyj/epos/controller/login/LoginController.java
@@ -1,16 +1,15 @@
 package com.fsyj.epos.controller.login;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+/*@Controller
+@ResponseBody// 所有方法的数据直接返回给浏览器*/
+@RestController
 public class LoginController {
 
+//    @ResponseBody
     @RequestMapping("/test")
-    @ResponseBody
     String test() {
         return "this is init test.";
     }
